cb804f3a142bad33d633737a17eeaed8e00bdf68
John Doe <arina.soldan@gmail.com>
2026-03-03 17:26:17 +0200

    feat: add responses

---
+++ responses.py
@@ -0,0 +1,36 @@
+questions = {
+    "who_is_arina": {
+        "question": "Who is Arina?",
+        "answer": "Arina is a software development graduate from Haaga-Helia University of Applied Sciences, focused on growing her skills and starting her professional journey in tech."
+    },
+
+    "skills": {
+        "question": "What technologies does she use?",
+        "answer": "Arina’s main focus is Java and REST API development. She has practical experience with API integrations, SQL, and frontend development using React. She has also worked on implementing AI-related features. Arina is open to different IT roles, especially those involving backend development, integrations, and system collaboration."
+    },
+
+    "projects": {
+        "question": "What projects has she built?",
+        "answer": "Arina built Java Spring Boot REST API for an appointment booking system using MongoDB. She also contributed to a full-stack AI project with React and Django, integrating external AI APIs and deploying services with Docker. Additionally, she developed a mobile movie app with a Java backend and third-party API integration, working in an Agile team as Scrum Master and Backend Developer."
+    },
+
+    "hire": {
+        "question": "Why should we hire her?",
+        "answer": "Arina learns fast, enjoys solving problems and is highly motivated to grow as a developer."
+    },
+
+    "office": {
+        "question": "Is she open to onsite work?",
+        "answer": "Yes! Arina is open to both remote and onsite opportunities."
+    }
+}
+
+
+fallback_messages = [
+    "That sounds fascinating, but maybe try one of the prepared questions about Arina.",
+    "Hmm... I'm not sure about that, try one of the prepared questions.",
+    "Fun question! But try to ask something from the list 😉"
+]
+
+
+final_fallback = "Unfortunately I don't understand the question. Please choose one of the prepared questions."
